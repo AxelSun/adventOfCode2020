7905b6fd6dbdbadb13652b7f77e2da7eb7914029
AxelSun <lga.sundberg@gmail.com>
2020-12-05 17:47:46 +0100

    Binaries are fun fun

--- src/main/scala/puzzles/Day5.scala
+++ src/main/scala/puzzles/Day5.scala
@@ -1,45 +1,32 @@
 package adventofcode.puzzles
-
-
 import adventofcode.SantasLittleHelper
 
 object Day5 extends SantasLittleHelper {
 
   def run(): Unit = {
-
     val seatIds = readInput("/day5.txt")
-      .map(_.splitAt(7))
-      .map(boardingPass => rowNbr(boardingPass._1) * 8 + seatNbr(boardingPass._2))
+        .map( boardingPass => {
+          boardingPass.map(c => if(c == 'F' || c == 'L') 0 else 1).splitAt(7)
+        })
+        .map(s => findNbrFromBits(s._1) * 8 + findNbrFromBits(s._2, 7))
 
     val maxSeatId = seatIds.max
     printFirstAnswer(maxSeatId.toString)
 
-    val allSeats = seatIds.min to maxSeatId
-    val mySeat = allSeats.toSet diff seatIds.toSet
-    printSecondAnswer(mySeat.toString)
+    val mySeat = (seatIds.min to seatIds.max).toSet diff seatIds.toSet
+    printSecondAnswer(mySeat.head.toString)
   }
 
-  def rowNbr(boardingPass: String): Int = {
-    def loop(s: List[Char], lower: Int, upper: Int): Int = s match {
+
+  def findNbrFromBits(bits: IndexedSeq[Int], upper: Int = 127): Int = {
+    def loop(bits: List[Int], lower: Int, upper: Int): Int = bits match {
       case head :: tail => {
         val idx = (lower + upper) / 2
-        if (head == 'F') loop(tail, lower, idx) else loop(tail, idx + 1, upper)
+        if (head == 0) loop(tail, lower, idx) else loop(tail, idx + 1, upper)
       }
       case Nil => lower
     }
-    loop(boardingPass.toList, 0, 127)
+    loop(bits.toList, 0, upper)
   }
-
-  def seatNbr(boardingPass: String): Int = {
-    def loop(s: List[Char], lower: Int, upper: Int): Int = s match {
-      case head::tail => {
-        val idx = (lower + upper) / 2
-        if (head == 'L') loop(tail, lower, idx) else loop(tail, idx + 1, upper)
-      }
-      case Nil => lower
-    }
-    loop(boardingPass.toList, 0, 7 )
-  }
-
 
 }
